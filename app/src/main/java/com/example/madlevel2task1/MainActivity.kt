e68347064b8fdca572df7558ed5d9c41137e4608
griends1 <Steelpan1>
2020-09-03 17:59:49 +0200

    Added item place layout, added array of places

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.FileReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
