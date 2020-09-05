d8dbb9b2737c9cab5cb7e6df3242666b4afb592b
griends1 <Steelpan1>
2020-09-05 14:47:39 +0200

    Implemented placeadapter methods

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.io.FileReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
